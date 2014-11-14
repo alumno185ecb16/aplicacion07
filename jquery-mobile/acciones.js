4d28204dfb7a65a12e51c2056a2137a755d7e487
alumno185ecb16 <jmialma05@gmail.com>
2014-11-14 09:02:55 -0600

    subida3

--- jquery-mobile/acciones.js
+++ jquery-mobile/acciones.js
@@ -3,11 +3,11 @@
 $(document).ready(function(e) {
     document.addEventListener("deviceready",function(){
 		
-		$('#izquierda').tap(function(){
+		$('#izquierda').on("swipeleft",function(){
 			navigator.notification.alert("Deslizó a la izquierda",function(){"Practica4","Aceptar"});
 });//barrer izquierda
 
-$('#derecha').swiperight(function(){
+$('#derecha').on("swiperight",function(){
 	navigator.notification.confirm("¿que quieres hacer?",function(opt){
 		switch(opt)
 		{
